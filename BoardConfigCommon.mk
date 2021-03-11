015ed920860afeca8f5227873dfdf33a308b0679
Hendra Manudinata <hendra32os@gmail.com>
2021-03-11 16:46:18 +0700

    universal7570: Mention toolchain
    Change-Id: I29209f89e723d6e28789ec0a7e4fbf71bdb6fb3c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,7 +55,8 @@ TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 
 # Kernel
 TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+KERNEL_TOOLCHAIN := $(BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 TARGET_LINUX_KERNEL_VERSION := 3.18
 
 # Kernel config
